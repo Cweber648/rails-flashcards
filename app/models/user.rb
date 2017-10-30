d2564d574faa75e28bd4e7f16fc98a625ad9d272
Charlie Weber & Matthew Black & Patrick Rodriguez <pair+cweber648.matthew-black.patrodriguez108@devbootcamp.com>
2017-10-30 12:31:39 -0500

    Seed database and ensure associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :rounds
-  has_many :guesses, through: :rounds
+  has_many :guesses, through: :rounds, source: :guesses
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  user = User.new(username: Faker::Internet.user_name(Faker::RickAndMorty.character), password: "password")
+  user.email = Faker::Internet.safe_email(user.username)
+  user.save
+end
+
+Deck.create(name: "Rails it Up Yo")
+
+Card.create(
+  question: "What is Rails tho?",
+  answer: "A server-side web app framework written in Ruby",
+  deck_id: 1
+  )
+Card.create(
+  question: "What do you type in the terminal to view routes?",
+  answer: "rake routes",
+  deck_id: 1
+  )
+Card.create(
+  question: "What year was Rails created_at?",
+  answer: "2005",
+  deck_id: 1
+  )
+Card.create(
+  question: "Roughly how many people have contributed to Rails?",
+  answer: "Over 4,500",
+  deck_id: 1
+  )
+Card.create(
+  question: "What is the containing folder of css and js files",
+  answer: "Assets",
+  deck_id: 1
+  )
+
+Round.create(user_id: 1, deck_id: 1)
+
+Guess.create(is_correct: true, card_id: 1, round_id: 1)
+Guess.create(is_correct: false, card_id: 2, round_id: 1)
+Guess.create(is_correct: false, card_id: 3, round_id: 1)
+Guess.create(is_correct: true, card_id: 4, round_id: 1)
+Guess.create(is_correct: true, card_id: 5, round_id: 1)
+Guess.create(is_correct: true, card_id: 2, round_id: 1)
+Guess.create(is_correct: true, card_id: 3, round_id: 1)
+
